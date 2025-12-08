4b772c6aa6249881514bd3f5d9946e07c20cc610
Tom Bulsink <nospam@sorrowed.nl>
2025-12-08 22:01:13 +0000

    WIP Day 02

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 const Advent_2025 = @import("Advent_2025");
 const day1 = @import("day01");
+const day2 = @import("day02");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("Advent 2025 in Zig\n", .{});
     try day1.part1();
     try day1.part2();
+    try day2.part1();
+    try day2.part2();
 }
 
 test {
